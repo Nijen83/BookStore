bdabf55022702080bfa1e2546e159fd221bc8246
UnknownRussian <37229726+UnknownRussian@users.noreply.github.com>
2022-12-22 10:58:18 +0100

    Fixed Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
     int orderId;
+    List<Book> listOfItems;
+
+    public Cart(int orderId){
+        this.orderId = orderId;
+        listOfItems = new ArrayList<>();
+    }
+
+    public void add(Book book){
+        listOfItems.add(book);
+    }
+
+    public void remove(Book book){
+        listOfItems.remove(book);
+    }
+
+    public void displayCart(){
+        System.out.println("--- Order list of items ---");
+        for(Book book : listOfItems){
+            book.printBook();
+        }
+    }
 }
