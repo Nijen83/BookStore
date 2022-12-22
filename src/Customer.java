65cbc93eeeb2e04aec090d8dd307981066a9f8fe
lisa watts <lisa.wts93@gmail.com>
2022-12-22 10:23:37 +0100

    Customer change

---
+++ src/Customer.java
@@ -0,0 +1,48 @@
+public class Customer {
+
+    int CustomerId;
+    String FirstName;
+    String LastName;
+    String Email;
+    int Phone;
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int phone) {
+        Phone = phone;
+    }
+}
